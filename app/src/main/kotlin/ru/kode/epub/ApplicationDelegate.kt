e708496ad74859d935a6d4cd31dea1ee2d5c0504
Alexander Ivanov <iv@kode.ru>
2026-04-20 08:37:00 +0200

    Connect firebase crashlytics

--- app/src/main/kotlin/ru/kode/epub/ApplicationDelegate.kt
+++ app/src/main/kotlin/ru/kode/epub/ApplicationDelegate.kt
@@ -1,6 +1,8 @@
 package ru.kode.epub
 
 import android.app.Application
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import ru.kode.epub.di.AppComponent
 import ru.kode.epub.di.AppComponentHolder
 import ru.kode.epub.di.create
@@ -27,6 +29,16 @@ class ApplicationDelegate : Application(), AppComponentHolder {
     if (BuildConfig.DEBUG) {
       Timber.plant(Timber.DebugTree())
       Timber.d("Timber initialized")
+    } else {
+      Timber.plant(
+        object : Timber.Tree() {
+          override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            val crashlytics = FirebaseCrashlytics.getInstance()
+            if (priority >= Log.WARN) crashlytics.log("priority=$priority, tag=$tag, message=$message")
+            t?.let(crashlytics::recordException)
+          }
+        }
+      )
     }
   }
 }
