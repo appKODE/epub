ac5660a84151235862d88c7158d734d6de577c8c
Sergey Shevtsov <ssh@kode.ru>
2026-04-20 13:56:24 +0200

    Add system paddings to ModalBottomSheet

--- core/uikit/src/main/kotlin/ru/kode/epub/core/uikit/component/ModalBottomSheet.kt
+++ core/uikit/src/main/kotlin/ru/kode/epub/core/uikit/component/ModalBottomSheet.kt
@@ -4,17 +4,18 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.union
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import ru.kode.epub.core.uikit.theme.AppTheme
 
@@ -25,13 +26,15 @@ fun ModalBottomSheet(
   showHandle: Boolean = true,
   sheetContent: @Composable ColumnScope.() -> Unit
 ) {
-  val topPadding = WindowInsets.statusBars
-    .union(WindowInsets.displayCutout)
-    .asPaddingValues()
-    .calculateTopPadding() + 10.dp
+  val safeInsets = WindowInsets.safeDrawing.asPaddingValues()
   val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+  val layoutDirection = LocalLayoutDirection.current
   ModalBottomSheet(
-    modifier = modifier.padding(top = topPadding),
+    modifier = modifier.padding(
+      start = safeInsets.calculateStartPadding(layoutDirection) + 10.dp,
+      top = safeInsets.calculateTopPadding() + 10.dp,
+      end = safeInsets.calculateEndPadding(layoutDirection) + 10.dp
+    ),
     containerColor = AppTheme.colors.surfaceLayer1,
     shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
     dragHandle = if (showHandle) {
